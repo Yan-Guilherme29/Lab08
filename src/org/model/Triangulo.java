8d488c6905a3d1ac6fc94b443a70a7a174739e0d
poo2-p1 <yanguilherme2927@gmail.com>
2025-11-05 14:43:44 -0300

    Classe Main adicionada e alteração na classe Triângulo

--- src/org/model/Triangulo.java
+++ src/org/model/Triangulo.java
@@ -5,6 +5,11 @@ public class Triangulo extends Forma{
     private double base;
     private double altura;
 
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
     @Override
     public double calcularArea() {
 
--- src/org/principal/Main.java
+++ src/org/principal/Main.java
@@ -1,4 +1,27 @@
 package org.principal;
 
+import org.model.Circulo;
+import org.model.Forma;
+import org.model.Quadrado;
+import org.model.Triangulo;
+
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        // Cria um ArrayList que armazena objetos do tipo Forma
+        ArrayList<Forma> formas = new ArrayList<>();
+
+        // Adiciona diferentes formas ao ArrayList
+        formas.add(new Circulo(5.0));
+        formas.add(new Quadrado(4.0));
+        formas.add(new Triangulo(6.0, 3.0));
+
+        // Percorre a lista e chama os métodos polimórficos
+        for (Forma f : formas) {
+            f.desenhar();
+            System.out.println("Área: " + f.calcularArea());
+            System.out.println("-----------------------");
+        }
+    }
 }
